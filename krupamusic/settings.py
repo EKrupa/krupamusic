5b8bddb6bd6b064bb753fc75ae7b20b730db43da
Evan Krupa <evanhkrupa@gmail.com>
2025-08-17 08:36:19 -0400

    Update settings.py

--- krupamusic/settings.py
+++ krupamusic/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'krupamusic.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
+        'NAME':env('DB_NAME'),
+        'USER':env('DB_USER'),
+        'PASSWORD':env('DB_PASSWORD'),
         'HOST':env('localhost'),
         'PORT': ''
 
